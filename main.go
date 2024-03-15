d3ca715ffcceea5d3944e26a1e1f855d7bd683ec
KIMSIHUN <tlgns14@nate.com>
2024-03-15 14:17:28 +0900

    🔥

--- main.go
+++ main.go
@@ -7,49 +7,51 @@ import (
 
 	"gofcm/config"
 
-	firebase "firebase.google.com/go/v4"
-	"firebase.google.com/go/v4/messaging"
+	firebase "firebase.google.com/go"
+	"firebase.google.com/go/messaging"
+	"google.golang.org/api/option"
 )
 
-func main()  {
+func main() {
 	firebaseConfig, err := config.ReadFirebaseConfig("firebaseconfig.json")
 	if err != nil {
 		log.Fatalf("error reading firebase config: %v\n", err)
 	}
-	app := initializeAppWithServiceAccount(firebaseConfig)
-	sendToToken(app)
-}
+	opt := option.WithCredentialsJSON([]byte(fmt.Sprintf(`{
+		"type": "%s",
+		"project_id": "%s",
+		"private_key_id": "%s",
+		"private_key": "%s",
+		"client_email": "%s",
+		"client_id": "%s",
+		"auth_uri": "%s",
+		"token_uri": "%s",
+		"auth_provider_x509_cert_url": "%s",
+		"client_x509_cert_url": "%s",
+	}`, firebaseConfig.Type, firebaseConfig.ProjectId, firebaseConfig.PrivateKeyId, firebaseConfig.PrivateKey, firebaseConfig.ClientEmail, firebaseConfig.ClientId, firebaseConfig.AuthUri, firebaseConfig.TokenUri, firebaseConfig.AuthProviderX509CertUrl, firebaseConfig.ClientX509CertUrl)))
 
-func sendToToken(app *firebase.App) {
-	ctx := context.Background()
-	client, err := app.Messaging(ctx)
-	if err != nil {
-		log.Fatalf("error getting Messaging client: %v\n", err)
-	}
-
-	registrationToken := "YOUR_REGISTRATION_TOKEN"
-
-	message := &messaging.Message{
-		Data: map[string]string{
-			"score": "850",
-			"time":  "2:45",
-		},
-		Token: registrationToken,
-	}
-
-	response, err := client.Send(ctx, message)
-	if err != nil {
-		log.Fatalln(err)
-	}
-	fmt.Println("Successfully sent message:", response)
-}
-
-func initializeAppWithServiceAccount(opt *config.FirebaseConfig) *firebase.App {
-	
 	app, err := firebase.NewApp(context.Background(), nil, opt)
 	if err != nil {
 		log.Fatalf("error initializing app: %v\n", err)
 	}
 
-	return app
+	client, err := app.Messaging(context.Background())
+	if err != nil {
+		log.Fatalf("error getting Messaging client: %v\n", err)
+	}
+
+	message := &messaging.Message{
+		Token: "ftnnBejlzk1HlvfZIW9GK0:APA91bFWmtw4wZPanV8iNtDZtQAYJFRs36sQsVKNO9lgAEz_OAiz5XCJVKv7u3yR1FDd9Uu9k8hzABcCHAu--VeQI_NXz1ZtORTK_Wmdy852MN-WfdlxLcvgRmyIMxmilCbQs49sompY",
+		Notification: &messaging.Notification{
+			Title: "FCM Test",
+			Body:  "This is a test notification",
+		},
+	}
+
+	response, err := client.Send(context.Background(), message)
+	if err != nil {
+		log.Fatalf("error sending message: %v\n", err)
+	}
+
+	fmt.Println("Successfully sent message:", response)
 }
